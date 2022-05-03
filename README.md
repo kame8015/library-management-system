19fd69fd79869f02090b00362c5f7b692836d456
kame <kame8015@gmail.com>
2022-05-03 18:23:11 +0900

    update application

--- README.md
+++ README.md
@@ -6,11 +6,54 @@
 $ python -m venv .venv
 $ source .venv/bin/activate
 $ pip install -r requirement_dev.txt
-$ pip install -r requirements.txt  # TODO
+$ pip install -r requirements.txt
 ```
 
 ## How to run Library Management System
 
 ```
-$ python app_main.py
+$ python app_main.py --books Test1 Test2 Test3        ?develop
+
+    =====LIBRARY MENU=====
+    1. Show all available books
+    2. Request a book
+    3. Return a book
+    4. Exit
+    ======================
+
+-> Enter a choice: 1
+<Library> Show all available books
+ID: 0, NAME: Test1
+ID: 1, NAME: Test2
+ID: 2, NAME: Test3
+
+-> Enter a choice: 2
+-> Enter an ID of book you wanna borrow: 0
+<Library> The book you requested has now been borrowed
+
+-> Enter a choice: 1
+<Library> Show all available books
+ID: 1, NAME: Test2
+ID: 2, NAME: Test3
+
+-> Enter a choice: 2
+-> Enter an ID of book you wanna borrow: 0
+<Library> Sorry, the book you have requested is not available
+
+[ERROR] Please enter the correct book ID
+-> Enter a choice: 3
+<Library> Show all lend books
+ID: 0, NAME: Test1
+
+-> Enter an ID of book you wanna return: 0
+<Library> Thanks for returning your borrowed book
+
+-> Enter a choice: 1
+<Library> Show all available books
+ID: 0, NAME: Test1
+ID: 1, NAME: Test2
+ID: 2, NAME: Test3
+
+-> Enter a choice: 4
+-> Thank you!
 ```
--- app_main.py
+++ app_main.py
@@ -1,104 +1,182 @@
+import argparse
+import itertools
 import sys
+from argparse import ArgumentParser
+from enum import Enum
+from typing import List, Optional
+
+BOOKS = ["The Last Battle1", "The Screwtape letters", "The Great Divorce"]
+
+
+class bookState(Enum):
+    AVAILABLE = "AVAILABLE"
+    RESERVATION = "RESERVATION"
+    LENDING = "LENDING"
+
+
+class Book:
+    new_id = itertools.count().__next__
+
+    def __init__(self, name, state: bookState, until_available: int = 0) -> None:
+        if not self.__validate(name, state, until_available):
+            raise Exception("Validation error in making instance of Book")
+        self.id = Book.new_id()
+        self.name = name
+        self.state = state
+        self.until_available = until_available
+
+    def __validate(self, name, state, until_available) -> bool:
+        if not isinstance(name, str):
+            return False
+
+        if not isinstance(state, bookState):
+            return False
+
+        if not isinstance(until_available, int):
+            return False
+
+        return True
 
 
 class Library:
-    def __init__(self, list_books: list):
-        self.availability_books = list_books
+    class Archive:
+        def __init__(self, books: List[Book]) -> None:
+            self.__book_list = books
 
-    def sort_availability_books(self):
-        self.availability_books.sort()
+        def __find_a_book(self, book_id: int) -> Optional[Book]:
+            book = next((x for x in self.__book_list if x.id == book_id), None)
+            return book
 
-    def lend_book(self, request_book: str):
-        print("")
-        if request_book in self.availability_books:
-            print("The book you requested has now been borrowed")
-            self.availability_books.remove(request_book)
-        else:
-            print("Sorry the book you have requested is currently not in the library")
-
-    def add_book(self, return_book):
-        if not return_book in self.availability_books:
-            self.availability_books.append(return_book)
+        def show_available_books(self) -> None:
+            print("<Library> Show all available books")
+            for book in self.__book_list:
+                if book.state == bookState.AVAILABLE:
+                    print(f"ID: {book.id}, NAME: {book.name}")
             print("")
-            print("Thanks for returning your borrowed book")
-        else:
+
+        def show_lending_books(self) -> None:
+            print("<Library> Show all lend books")
+            for book in self.__book_list:
+                if book.state == bookState.LENDING:
+                    print(f"ID: {book.id}, NAME: {book.name}")
             print("")
-            print("Sorry the book you have returned is currently in the library")
 
-    # def add_to_temp(self, return_book: str):
-    #     print("")
-    #     if not self.availability_books:
-    #         self.availability_books.remove(return_book)
-    #         print("Added to temporary")
+        def lend_a_book(self, book_id: int) -> bool:
+            book = self.__find_a_book(book_id=book_id)
+            if not book:
+                print(
+                    "<>Library Sorry, the book you have requested is currently not in the library"
+                )
+                print("")
+                return False
+            else:
+                if book.state == bookState.AVAILABLE:
+                    print("<Library> The book you requested has now been borrowed")
+                    print("")
+                    book.state = bookState.LENDING
+                    return True
+                else:
+                    print(
+                        "<Library> Sorry, the book you have requested is not available"
+                    )
+                    print("")
+                    return False
 
-    # def add_from_temp(self, ):
-    #     print("")
-    #     if
+        def add_a_book(self, book_id: int) -> bool:
+            book = self.__find_a_book(book_id=book_id)
+            if not book:
+                print(
+                    "<Library> Sorry, the book you tried to return does not belong to the library "
+                )
+                print("")
+                return False
+            else:
+                if book.state == bookState.LENDING:
+                    print("<Library> Thanks for returning your borrowed book")
+                    print("")
+                    book.state = bookState.AVAILABLE
+                    return True
+                else:
+                    print("<Library> Sorry, that book is not available for borrowing")
+                    print("")
+                    return False
 
 
-# class TemporaryLibrary:
-#     def __init__(self, list_books: list):
-#         self.reserved_books = list_books
+class User:
+    def __init__(self, archive: Library.Archive) -> None:
+        self.borrowed_book_list = []
+        self.archive = archive
 
-#     def remove(self, request):
-#         if request in self.reserved_books:
-#             self.reserved_books.remove(request)
+    def request_a_book(self) -> None:
+        try:
+            book_id = int(input("-> Enter an ID of book you wanna borrow: "))
+            ret = self.archive.lend_a_book(book_id=book_id)
+            if not ret:
+                raise Exception
+        except Exception:
+            err_msg = "[ERROR] Please enter the correct book ID"
+            print(err_msg)
 
-#     def add(self, request):
-#         if not request in self.reserved_books:
-#             self.reserved_books.append(request)
+    def return_a_book(self) -> None:
+        try:
+            self.archive.show_lending_books()
+            book_id = int(input("-> Enter an ID of book you wanna return: "))
+            ret = self.archive.add_a_book(book_id=book_id)
+            if not ret:
+                raise Exception
+        except Exception:
+            err_msg = "[ERROR] Please enter the correct book ID"
+            print(err_msg)
 
 
-class Student:
-    def request_book(self):
-        print("")
-        print("Enter the name of the book you'd like to borrow>>")
-        self.book = input()
-        return self.book
+def activate_library(archive: Library.Archive):
+    user = User(archive=archive)
 
-    def return_book(self):
-        print("")
-        print("Enter the name of the book you'd like to return>>")
-        self.book = input()
-        return self.book
+    main_msg = """
+    =====LIBRARY MENU=====
+    1. Show all available books
+    2. Request a book
+    3. Return a book
+    4. Exit
+    ======================
+    """
+    print(main_msg)
+    err_msg = "[ERROR] Please enter the correct choice"
+
+    while True:
+        try:
+            choice = int(input("-> Enter a choice: "))
+        except Exception:
+            print(err_msg)
+            continue
+        if choice == 1:
+            archive.show_available_books()
+        elif choice == 2:
+            user.request_a_book()
+        elif choice == 3:
+            user.return_a_book()
+        elif choice == 4:
+            print("-> Thank you!")
+            sys.exit()
+        else:
+            print(err_msg)
 
 
 def main():
-    BOOKS = ["The Last Battle1", "The Screwtape letters", "The Great Divorce"]
-    library = Library(BOOKS)
-    student = Student()
-    print(
-        "=====LIBRARY MENU=====\n"
-        + "1. Request a book\n"
-        + "2. Return a book\n"
-        + "3. Exit\n"
-        + "======================"
+    parser = ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="Please run and initialize along with the list of books",
     )
-    while True:
-        print("")
-        print("-----The books in library-----")
-        library.sort_availability_books()  # sort by name
-        books = library.availability_books
-        for book in books:
-            print(book)
-        print("------------------------------")
-        print("")
-
-        try:
-            choice = int(input("Enter Choice: "))
-        except Exception:
-            print("[ERROR] Please enter the correct choice")
-            continue
-        if choice == 1:
-            library.lend_book(student.request_book())
-        elif choice == 2:
-            library.add_book(student.return_book())
-        elif choice == 3:
-            print("Bye")
-            sys.exit()
-        else:
-            print("[ERROR] Please enter the correct choice")
-            print("")
+    parser.add_argument("--books", nargs="*", required=True, help="list of books")
+    args = parser.parse_args()
+    # print(args.books)
+    books = []
+    for i in args.books:
+        book = Book(name=i, state=bookState.AVAILABLE)
+        books.append(book)
+    library = Library()
+    archive = library.Archive(books=books)
+    activate_library(archive=archive)
 
 
 if __name__ == "__main__":
